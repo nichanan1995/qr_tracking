5ef5ad65b21c9a54815c0a132e2df33f9ad137ed
nichanan <nichanan.j@sunteen.co.th>
2019-03-23 00:20:47 +0700

    qr scanner

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:qrcode_reader/qrcode_reader.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'QRCode Tracking NO.',
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  final Map<String, dynamic> pluginParameters = {};
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _barcodeString;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Counter Tracking NO.'),
+      ),
+      body: new Center(
+          child: new FutureBuilder<String>(
+              future: _barcodeString,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                return new Text(snapshot.data != null ? snapshot.data : '');
+              })),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _barcodeString = new QRCodeReader()
+                .setAutoFocusIntervalInMs(200)
+                .setForceAutoFocus(true)
+                .setTorchEnabled(true)
+                .setHandlePermissions(true)
+                .setExecuteAfterPermissionGranted(true)
+                .scan();
+          });
+        },
+        tooltip: 'Reader Tracking NO.',
+        child: new Icon(Icons.scanner),
+      ),
+    );
+  }
+}
